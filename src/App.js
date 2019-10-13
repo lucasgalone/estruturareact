83f1d2db06ef0ef304ec813dbea3ea25ea2b86cc
Lucas Vinicius Galone <lucas.vinicius.galone@hotmail.com>
2019-10-13 18:10:38 -0300

    inicia app web matheducation login

--- src/App.js
+++ src/App.js
@@ -1,10 +1,24 @@
-import React from "react";
+import React from 'react';
+import { Provider } from 'react-redux';
+import { Router } from 'react-router-dom';
+
+import './config/ReactotronConfig';
+
+import Routes from './routes';
+import history from './services/history';
+
+import store from './store';
+
+import GlobalStyle from './styles/global';
 
 function App() {
   return (
-    <div className="App">
-      <h1>Hello Word</h1>
-    </div>
+    <Provider store={store}>
+      <Router history={history}>
+        <Routes />
+        <GlobalStyle />
+      </Router>
+    </Provider>
   );
 }
 
